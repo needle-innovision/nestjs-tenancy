f7086e3d47e03789a45e505815165863b2f6c5b3
Adam Jessop <adam@endpoint.gg>
2022-06-24 16:28:08 +0100

    More descriptive guard error

--- lib/guards/tenant-guard.ts
+++ lib/guards/tenant-guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 export class TenantGuard implements CanActivate {
@@ -6,6 +6,10 @@ export class TenantGuard implements CanActivate {
 
     public canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const request = context.switchToHttp().getRequest();
-        return this.needsTenant && !!request.tenantId;
+        if (this.needsTenant && !request.tenantId) {
+            throw new BadRequestException('Tenant ID is mandatory');
+        }
+
+        return true;
     }
 }
